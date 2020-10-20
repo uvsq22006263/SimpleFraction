62e0732060d5bf3c616264a660906a75758a4316
Aymen Ben Tanfous <aymen.bentanfous1@gmail.com>
2020-10-20 21:26:17 +0200

    add toString methode

--- src/main/java/Fraction.java
+++ src/main/java/Fraction.java
@@ -1,2 +1,13 @@
 public class Fraction {
+
+    public int x;
+
+    public Fraction(int x) {
+        this.x = x;
+    }
+
+    public String toString() {
+        return "x = " + x;
+    }
+
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,7 +2,8 @@ public class main {
 
 
     public static void main(String[] args) {
-
         System.out.println("hello");
+        Fraction f =new Fraction(5);
+        System.out.println(f.toString());
     }
 }
